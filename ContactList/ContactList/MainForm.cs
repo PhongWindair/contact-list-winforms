7bf251757d426aaff097c81b8a93a74529aa3956
PhongWindair <tranphong120560@gmail.com>
2025-05-27 08:35:30 +0700

    Khởi tạo dự án Windows Forms với MainForm

--- ContactList/ContactList/MainForm.cs
+++ ContactList/ContactList/MainForm.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace ContactList
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
